03854b58f5c3f529b2043cda725c66ae5d1447a7
zdw <1732182169@qq.com>
2021-04-07 15:44:41 +0800

    第一次提交

---
+++ GrpcGreeter/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcGreeter.Services
+{
+    public class UserService : Greeter.GreeterBase
+    {
+        private readonly ILogger<UserService> _logger;
+        public UserService(ILogger<UserService> logger)
+        {
+            _logger = logger;
+        }
+
+        public  Task<HelloReply> GetUser(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        /// <summary>
+        /// 异步主方法
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static async Task Main(string[] args)
+        {
+            ////创建通道
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //创建客户端
+            var client = new Greeter.GreeterClient(channel);
+            //调用客户方法
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            //打印
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            //创建通道[user不可调用，依据官网和各路大神的方法，都不行，诶，凉凉]
+            //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            ////创建客户端
+            //var client = new User.UserClient(channel);
+            ////调用客户方法
+            //var reply = await client.GetUser(new HelloRequest { Name = "123" });
+            ////打印
+            //Console.WriteLine("姓名: " + reply.Message);
+            //Console.WriteLine("Press any key to exit...");
+            //Console.ReadKey();
+        }
+    }
+}
